15c06aafbf5e944d2dd0c20e2a7d7e6f5d8f01cf
Jae Min Kim <kjm91806@gmail.com>
2025-11-23 15:27:30 +0900

    refactor: 기존에 SavingsCalculatorPage에 존재하던 컴포넌트를 도메인 폴더 내 Calculation으로 분리

--- src/pages/SavingsCalculatorPage.tsx
+++ src/pages/SavingsCalculatorPage.tsx
@@ -1,5 +1,6 @@
+import { Calculation } from 'domains/calculation';
 import { SavingsProducts } from 'domains/savings-products';
-import { Border, NavigationBar, SelectBottomSheet, Spacing, Tab, TextField } from 'tosslib';
+import { Border, NavigationBar, Spacing, Tab } from 'tosslib';
 
 export function SavingsCalculatorPage() {
   return (
@@ -8,15 +9,7 @@ export function SavingsCalculatorPage() {
 
       <Spacing size={16} />
 
-      <TextField label="목표 금액" placeholder="목표 금액을 입력하세요" suffix="원" />
-      <Spacing size={16} />
-      <TextField label="월 납입액" placeholder="희망 월 납입액을 입력하세요" suffix="원" />
-      <Spacing size={16} />
-      <SelectBottomSheet label="저축 기간" title="저축 기간을 선택해주세요" value={12} onChange={() => {}}>
-        <SelectBottomSheet.Option value={6}>6개월</SelectBottomSheet.Option>
-        <SelectBottomSheet.Option value={12}>12개월</SelectBottomSheet.Option>
-        <SelectBottomSheet.Option value={24}>24개월</SelectBottomSheet.Option>
-      </SelectBottomSheet>
+      <Calculation />
 
       <Spacing size={24} />
       <Border height={16} />
